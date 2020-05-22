fe54dcd8106f307bb995f1fffeaca5e24ce4a662
Aihimieid900 <aihimieid900@gmail.com>
2020-05-22 04:26:02 +0200

    first change Categories with chat

---
+++ android/app/src/profile/gen/com/smartervision/ecommerce_app_ui_kit/ecommerce_app_ui_kit/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.smartervision.ecommerce_app_ui_kit.ecommerce_app_ui_kit;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
